a27cacfcb1b9e159f76a6f3e9156e7fed6cc9c11
Yago <dolmayan@gmail.com>
2024-07-02 13:24:45 +0200

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let reversedString = "";
 
+    for (let i = text.length - 1; i >= 0; i--) {
+        reversedString = reversedString + text[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
