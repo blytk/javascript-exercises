ab9dc6766c24f3de27c9fe5ab881ec9c6f0d2150
Yago <dolmayan@gmail.com>
2024-07-02 20:22:51 +0200

    COmplete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  for (let i = 0, length = array.length; i < length; i++) {
+    total = total + array[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let i = 0, length = array.length; i < length; i++) {
+    total = total * array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+
+  for (let i = a; i > 0; i--) {
+    total = total * i;
+  }
+  
+  return total;
 };
 
 // Do not edit below this line
