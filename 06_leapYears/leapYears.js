af98540197a7623b32193435381ab5e433faf28b
Yago <dolmayan@gmail.com>
2024-07-02 19:18:48 +0200

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+// Given year: leap year?
+const leapYears = function(year) {
+    // divisible by 4? leap year
+    if (year % 4 === 0) {
+        // divisible by 100? Only leap if divisible by 400 too
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
