c8411a0f2e4930adca1ce5a1265c879f5bbc445f
Yago <dolmayan@gmail.com>
2024-07-02 19:35:31 +0200

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// F to C
+  // ºF = (x - 32) * 5/9 ºC
+const convertToCelsius = function(fahrenheitDegrees) {
+  const celsiusDegrees = (fahrenheitDegrees - 32) * (5 / 9);
+  return Math.round(celsiusDegrees * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// (x * 9 / 5 + 32 ºF)
+const convertToFahrenheit = function(celsiusDegrees) {
+  const fahrenheitDegrees = (celsiusDegrees * (9 / 5) + 32);
+  return Math.round(fahrenheitDegrees * 10) / 10;
+
 };
 
 // Do not edit below this line
