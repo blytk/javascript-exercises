b02763b434522938db3cfe7594c0bc5ae24a0aef
Yago <dolmayan@gmail.com>
2024-07-03 13:16:18 +0200

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
-
+// function takes the array and returns an array of titles
+const getTheTitles = function(bookArray) {
+    // create new array to add the titles
+    const titleArray = [];
+    // iterate through all elements of input array, and push the title to the new array
+    for (const book of bookArray) {
+        // add the title to the new array to be returned
+        titleArray.push(book.title);
+    }
+    
+    return titleArray;
 };
 
 // Do not edit below this line
